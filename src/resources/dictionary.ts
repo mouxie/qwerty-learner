91ef6cb936b4aa5162168786f3422425bd1250d2
mouxie <620710+mouxie@users.noreply.github.com>
2025-05-14 10:20:00 +0800

    Update dictionary.ts

--- src/resources/dictionary.ts
+++ src/resources/dictionary.ts
@@ -32,7 +32,7 @@ const chinaExam: DictionaryResource[] = [
     category: '中国考试',
     tags: ['大学英语'],
     url: '/dicts/dictionary.json',
-    length: 1494,
+    length: 1455,
     language: 'en',
     languageCategory: 'en',
   },
