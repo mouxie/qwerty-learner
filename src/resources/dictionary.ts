832351a036e6970c5069934d0e4b0c809d0d790d
mouxie <620710+mouxie@users.noreply.github.com>
2025-08-25 09:05:43 +0800

    Update dictionary.ts

--- src/resources/dictionary.ts
+++ src/resources/dictionary.ts
@@ -32,7 +32,7 @@ const chinaExam: DictionaryResource[] = [
     category: '中国考试',
     tags: ['大学英语'],
     url: '/dicts/dictionary.json',
-    length: 1403,
+    length: 1401,
     language: 'en',
     languageCategory: 'en',
   },
