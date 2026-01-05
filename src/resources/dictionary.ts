6cf705b4e9136d2216c6c004628c0185a0a826e2
mouxie <620710+mouxie@users.noreply.github.com>
2026-01-05 15:00:19 +0800

    Update dictionary length from 1687 to 2883

--- src/resources/dictionary.ts
+++ src/resources/dictionary.ts
@@ -32,7 +32,7 @@ const chinaExam: DictionaryResource[] = [
     category: '中国考试',
     tags: ['大学英语'],
     url: '/dicts/dictionary.json',
-    length: 1687,
+    length: 2883,
     language: 'en',
     languageCategory: 'en',
   },
